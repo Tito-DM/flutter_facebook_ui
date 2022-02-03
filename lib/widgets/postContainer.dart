9a80d580ac60f33feb5085dff58e062ef5a9592a
pyong <muanda1993@hotmail.com>
2022-02-03 01:56:46 +0000

    complete post session

--- lib/widgets/postContainer.dart
+++ lib/widgets/postContainer.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class PostContainer extends StatelessWidget {
   final Post post;
@@ -38,7 +40,13 @@ class PostContainer extends StatelessWidget {
                   padding: EdgeInsets.symmetric(vertical: 8.0),
                   child: CachedNetworkImage(imageUrl: post.imageUrl),
                 )
-              : const SizedBox.shrink()
+              : const SizedBox.shrink(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: _PostStats(
+              post: post,
+            ),
+          )
         ]));
   }
 }
@@ -79,5 +87,119 @@ class _PostHeader extends StatelessWidget {
         IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz))
       ],
     );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+  const _PostStats({Key key, @required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                  color: Palette.facebookBlue, shape: BoxShape.circle),
+              child: const Icon(
+                Icons.thumb_up,
+                size: 10.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              width: 4.0,
+            ),
+            Expanded(
+              child: Text(
+                '${post.likes}',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
+            Text(
+              '${post.comments} comment',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Text(
+              '${post.shares} shares',
+              style: TextStyle(color: Colors.grey[600]),
+            )
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: [
+            _PostBtn(
+                icon: Icon(
+                  MdiIcons.thumbUpOutline,
+                  color: Colors.grey[600],
+                ),
+                label: 'like',
+                onTap: () => print("like")),
+               
+                _PostBtn(
+                icon: Icon(
+                  MdiIcons.commentOutline,
+                  color: Colors.grey[600],
+                ),
+                label: 'comment',
+                onTap: () => print("comment")),
+
+                _PostBtn(
+                icon: Icon(
+                  MdiIcons.shareOutline,
+                  color: Colors.grey[600],
+                  size: 25.0,
+                ),
+                label: 'share',
+                onTap: () => print("share")),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _PostBtn extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final Function onTap;
+
+  const _PostBtn(
+      {Key key,
+      @required this.icon,
+      @required this.label,
+      @required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Material(
+            color: Colors.white,
+            child: //add material to inkwell to have ink effect
+                InkWell(
+              onTap: onTap,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                height: 25.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    icon,
+                    const SizedBox(
+                      width: 4.0,
+                    ),
+                    Text(label),
+                  ],
+                ),
+              ),
+            )));
   }
 }
